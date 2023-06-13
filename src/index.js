248abbc161bceed0038112a4de2fe3fb9aa08ed8
Eyob Alemu <it.eyob.alemu@gamil.com>
2023-06-13 14:01:00 +0300

    add users tab

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = "http://localhost:8080/api";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
